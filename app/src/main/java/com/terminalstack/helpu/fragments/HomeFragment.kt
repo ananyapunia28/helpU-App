1447acd33c037019260e48500cd15726e94bf02b
shivenducs1136 <77795429+shivenducs1136@users.noreply.github.com>
2022-04-10 16:59:50 +0530

    ExploreNewOppotunity is clickable now

--- app/src/main/java/com/terminalstack/helpu/fragments/HomeFragment.kt
+++ app/src/main/java/com/terminalstack/helpu/fragments/HomeFragment.kt
@@ -1,11 +1,11 @@
 package com.terminalstack.helpu.fragments
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
-import android.view.Gravity
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.drawerlayout.widget.DrawerLayout
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.terminalstack.helpu.MainActivity
@@ -84,5 +84,12 @@ class HomeFragment : Fragment() {
             findNavController().navigate(R.id.action_homeFragment_to_helpDeskFragment)
         }
 
+        binding.goiExplore.setOnClickListener {
+            val uri: Uri =
+                Uri.parse("https://www.disabilityaffairs.gov.in/content/") // missing 'http://' will cause crashed
+
+            val intent = Intent(Intent.ACTION_VIEW, uri)
+            startActivity(intent)
+        }
     }
 }
